92531053e32c2672d2ba05dbce9ffaaaa72ff8ef
Divine Chukwudumebi Okorie <96950772+didi168@users.noreply.github.com>
2023-05-20 11:31:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -57,3 +57,20 @@ function closeMenu(){
   document.getElementsByClassName("menu")[0].style.top="-5%";
 }
 
+
+
+
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+  
+    } else {
+      entry.target.classList.remove('show')
+    }
+  });
+});
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
