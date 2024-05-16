dd56edb46f64d5547db27e9c6aadd154a0171115
DiDi168 <divineoko2018@gmail.com>
2024-05-16 22:05:27 +0100

    new look !!!!!!

--- script.js
+++ script.js
@@ -1,76 +1,118 @@
-function graph(){
-  document.getElementsByClassName("graphicsDiv")[0].style.display="block";
-  document.getElementsByClassName("uxDiv")[0].style.display="none";
-  document.getElementsByClassName("webDiv")[0].style.display="none";
-  document.getElementsByClassName("graph")[0].style.background="var(--red)";
-  document.getElementsByClassName("graph")[0].style.color="white";
-  document.getElementsByClassName("web")[0].style.color="black";
-  document.getElementsByClassName("web")[0].style.background="transparent";
-  document.getElementsByClassName("ui")[0].style.background="transparent"
-  document.getElementsByClassName("ui")[0].style.color="black"
-}
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
 
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
 
-function ui(){
-  document.getElementsByClassName("graphicsDiv")[0].style.display="none";
-  document.getElementsByClassName("uxDiv")[0].style.display="flex";
-  document.getElementsByClassName("webDiv")[0].style.display="none";
-  document.getElementsByClassName("graph")[0].style.background="transparent";
-  document.getElementsByClassName("graph")[0].style.color="black";
-  document.getElementsByClassName("web")[0].style.color="black";
-  document.getElementsByClassName("web")[0].style.background="transparent";
-  document.getElementsByClassName("ui")[0].style.background="var(--red)"
-  document.getElementsByClassName("ui")[0].style.color="white"
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
 
-}
-function web(){
-  document.getElementsByClassName("graphicsDiv")[0].style.display="none";
-  document.getElementsByClassName("uxDiv")[0].style.display="none";
-  document.getElementsByClassName("webDiv")[0].style.display="flex";
-  document.getElementsByClassName("graph")[0].style.background="transparent";
-  document.getElementsByClassName("graph")[0].style.color="black";
-  document.getElementsByClassName("web")[0].style.color="white";
-  document.getElementsByClassName("web")[0].style.background="var(--red)";
-  document.getElementsByClassName("ui")[0].style.background="transparent"
-  document.getElementsByClassName("ui")[0].style.color="black"
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
 
-}
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Developer", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
 
-// document.onreadystatechange = function () {
-//   document.querySelector("body").style.visibility = "none";
-//   if (document.readyState !== "complete") {
-//     document.querySelector("body").style.visibility = "none";
+    var typed = new Typed(".typing-2", {
+        strings: ["Developer", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
 
-//     document.querySelector(".loader").style.visibility = "visible";
-//   } else {
-//     document.querySelector(".loader").style.display = "none";
-
-//     document.querySelector("body").style.visibility = "visible";
-//   }
-// };
-
-function openMenu(){
-  document.getElementsByClassName("menu")[0].style.top="0%";
-}
-
-function closeMenu(){
-  document.getElementsByClassName("menu")[0].style.top="-5%";
-}
-
-
-
-
-
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry)
-    if (entry.isIntersecting) {
-      entry.target.classList.add('show');
-  
-    } else {
-      entry.target.classList.remove('show')
-    }
-  });
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
 });
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
+
+
+
+
+
+
+
+
+function graph(){
+    document.getElementsByClassName("graphicsDiv")[0].style.display="block";
+    document.getElementsByClassName("uxDiv")[0].style.display="none";
+    document.getElementsByClassName("webDiv")[0].style.display="none";
+    document.getElementsByClassName("graph")[0].style.background="var(--orange)";
+    document.getElementsByClassName("graph")[0].style.color="white";
+    document.getElementsByClassName("web")[0].style.color="black";
+    document.getElementsByClassName("web")[0].style.background="transparent";
+    document.getElementsByClassName("ui")[0].style.background="transparent"
+    document.getElementsByClassName("ui")[0].style.color="black"
+  }
+  
+  
+  function ui(){
+    document.getElementsByClassName("graphicsDiv")[0].style.display="none";
+    document.getElementsByClassName("uxDiv")[0].style.display="flex";
+    document.getElementsByClassName("webDiv")[0].style.display="none";
+    document.getElementsByClassName("graph")[0].style.background="transparent";
+    document.getElementsByClassName("graph")[0].style.color="black";
+    document.getElementsByClassName("web")[0].style.color="black";
+    document.getElementsByClassName("web")[0].style.background="transparent";
+    document.getElementsByClassName("ui")[0].style.background="var(--orange)"
+    document.getElementsByClassName("ui")[0].style.color="white"
+  
+  }
+//   function web(){
+//     document.getElementsByClassName("graphicsDiv")[0].style.display="none";
+//     document.getElementsByClassName("uxDiv")[0].style.display="none";
+//     document.getElementsByClassName("webDiv")[0].style.display="flex";
+//     document.getElementsByClassName("graph")[0].style.background="transparent";
+//     document.getElementsByClassName("graph")[0].style.color="black";
+//     document.getElementsByClassName("web")[0].style.color="white";
+//     document.getElementsByClassName("web")[0].style.background="var(--red)";
+//     document.getElementsByClassName("ui")[0].style.background="transparent"
+//     document.getElementsByClassName("ui")[0].style.color="black"
+  
+//   }
